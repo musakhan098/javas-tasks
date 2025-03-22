0434b03e9094fb3470f02d5886f8b93c100d0280
musakhan098 <muhammadmusakhn1@gmail.com>
2025-03-22 13:27:30 +0500

    Add files via upload

---
+++ taskss/script.js
@@ -0,0 +1,76 @@
+
+        // async function process(){
+        //     const response = await fetch('https://catfact.ninja/fact');
+        //     const data = await response.json();
+        //     console.log(data);
+        //     console.log(data.fact)
+        //     const h1 = document.getElementById("container");
+        //     h1.textContent = data.fact
+        // }
+        // process();
+  
+        // async function process(){
+        //     const response = await fetch('https://api.agify.io?name=meelad');
+        //     const data = await response.json();
+        //     console.log(data);
+        //     console.log(data.name)
+        //     const h1 = document.getElementById("container");
+        //     h1.textContent = data.name
+        // }
+        // process();
+
+
+        // async function process(){
+        //     const response = await fetch('https://api.genderize.io?name=luc');
+        //     const data = await response.json();
+        //     console.log(data);
+        //     console.log(data.gender)
+        //     const h1 = document.getElementById("container");
+        //     h1.textContent = data.gender
+        // }
+        // process();
+
+
+    //   async function process() {
+    //     const response = await fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
+    //     const data = await response.json();
+    //     console.log(data)
+    //     console.log(data.data[1].Nation)
+    //     const h1 =document.getElementById("container");
+    //     h1.textContent = data.data[1].Nation
+        
+    //   }
+    //   process();
+
+
+    //    async function process() {
+    //     const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    //     const data = await response.json();
+    //     console.log(data)
+    //     console.log(data.status)
+    //     const h1 =document.getElementById("container");
+    //     h1.textContent = data.status
+        
+    //   }
+    //   process();
+
+    // async function process() {
+    //     const response = await fetch('https://api.ipify.org?format=json');
+    //     const data = await response.json();
+    //     console.log(data)
+    //     console.log(data.ip)
+    //     const h1 = document.getElementById("container");
+    //     h1.textContent = data.ip
+    //   }
+    //   process();
+
+
+    // async function process() {
+    //     const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+    //     const data = await response.json();
+    //     console.log(data)
+    //     console.log(data.setup)
+    //     const h1 = document.getElementById("container");
+    //     h1.textContent = data.setup
+    // }
+    // process();
